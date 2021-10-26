1ba1629920ff89da5fc45be742e958243c4964bc
Marat Faizerakhman <m.faizerakhman@portavita.eu>
2021-10-26 19:19:51 +0300

    done

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const Router = require('express')
 const router = new Router()
 const postRouter = require('./postRouter')
-const userRouter = require('./userRouter')
+// const userRouter = require('./userRouter')
 const telegramRouter = require('./telegramRouter')
 
-router.use('/user', userRouter)
+// router.use('/user', userRouter)
 router.use('/post', postRouter)
 router.use('/telegram', telegramRouter)
 
