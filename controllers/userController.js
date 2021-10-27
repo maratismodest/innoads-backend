d7d77405ab4f093964ea90272cace7d88cd66c7e
Marat Faizerakhman <m.faizerakhman@portavita.eu>
2021-10-27 17:04:56 +0300

    dd

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const {User} = require("../models/models");
+const {User, Tg} = require("../models/models");
 const {DataTypes} = require("sequelize");
 
 class UserController {
@@ -32,35 +32,39 @@ class UserController {
     }
 
     async postUser(req, res) {
-        try {
-            let {email, password, name, surname} = req.body
-            const user = await User.create({email, password, name, surname});
-            return res.json(user)
-        } catch (e) {
-            return res.json(e)
+        let {
+            auth_date,
+            first_name,
+            hash,
+            id,
+            last_name,
+            photo_url,
+            username
+        } = req.body
+        const [user, created] = await Tg.findOrCreate({
+            where: {id: id},
+            defaults: {
+                ...req.body
+            }
+        });
+        if (created) {
+            console.log(user.id);
+            return res.json(created)
         }
-
-
-        // id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
-        // email: {type: DataTypes.STRING, unique: true},
-        // password: {type: DataTypes.STRING},
-        // name: {type: DataTypes.STRING},
-        // surname: {type: DataTypes.STRING},
-        // telegram: {type: DataTypes.STRING, unique: true},
-        // phone: {type: DataTypes.STRING, unique: true},
-        // role: {type: DataTypes.STRING, defaultValue: "USER"},
-
-
-        // const user = await User.create({
-        //     email: "email",
-        //     password: "email",
-        //     name: "Inno",
-        //     surname: "Ads",
-        //     telegram: "",
-        //     phone: "+71234567890"
-        // })
+        return res.json(user)
 
     }
+
+    // async postUser(req, res) {
+    //     try {
+    //         let {email, password, name, surname} = req.body
+    //         const user = await User.create({email, password, name, surname});
+    //         return res.json(user)
+    //     } catch (e) {
+    //         return res.json(e)
+    //     }
+    //
+    // }
 
     // async getUser(req, res) {
     //     const {email} = req.query
