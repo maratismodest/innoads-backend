453ba2a5ac544356118284bdb41f2ce9e7603ae7
Marat Faizerakhmanov <maratismodest@gmail.com>
2022-06-03 17:19:05 +0300

    options.refactor

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ app.use("/", router);
 app.use(express.static(__dirname));
 
 // Certificate
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/chamala.tatar/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/chamala.tatar/fullchain.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/chamala.tatar/chain.pem', 'utf8');
+// const privateKey = fs.readFileSync('/etc/letsencrypt/live/chamala.tatar/privkey.pem', 'utf8');
+// const certificate = fs.readFileSync('/etc/letsencrypt/live/chamala.tatar/fullchain.pem', 'utf8');
+// const ca = fs.readFileSync('/etc/letsencrypt/live/chamala.tatar/chain.pem', 'utf8');
 
-const credentials = {
-    key: privateKey, cert: certificate, ca: ca
-};
+// const credentials = {
+//     key: privateKey, cert: certificate, ca: ca
+// };
 
 
 app.get("/", function (req, res) {
@@ -30,15 +30,15 @@ app.get("/", function (req, res) {
 
 // Starting both http & https servers
 const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
+// const httpsServer = https.createServer(credentials, app);
 
 const start = async () => {
     try {
         await sequelize.authenticate();
         await sequelize.sync();
 
-        httpServer.listen(80, () => console.log(`Server started on port ${80}`));
-        httpsServer.listen(443, () => console.log(`Server started on port ${443}`));
+        httpServer.listen(8080, () => console.log(`Server started on port ${8080}`));
+        // httpsServer.listen(443, () => console.log(`Server started on port ${443}`));
     } catch (e) {
         console.log(e);
     }
