e0a351c9e3670098a54d5d5a7a0e7506d048f596
Marat Faizerakhmanov <maratismodest@gmail.com>
2022-06-14 18:03:42 +0300

    Update postRouter.js

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -6,6 +6,6 @@ const { bearerMiddleware, sameUserMiddleware, isUsersPost } = require("../middle
 router.get('/', postController.getPosts)
 router.get('/:slug', postController.getOne)
 router.post('/', bearerMiddleware, sameUserMiddleware, postController.postPost)
-router.put('/', bearerMiddleware, sameUserMiddleware, postController.putPost)
+router.put('/', bearerMiddleware, isUsersPost, postController.putPost)
 router.delete('/:id', bearerMiddleware, isUsersPost, postController.deletePost)
 module.exports = router
