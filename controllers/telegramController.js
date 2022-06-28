82586f3b613069ed17a96687abf5d1b3e9a77c64
Marat Faizerakhmanov <maratismodest@gmail.com>
2022-06-28 10:03:12 +0300

    commit

--- controllers/telegramController.js
+++ controllers/telegramController.js
@@ -7,31 +7,21 @@ const aSequelize = require("../db");
 class TelegramController {
     async postTelegram(req, res) {
         try {
-            // const chat_id = "@innoadsstage"
-            const chat_id = "@innoads";
+            const chat_id = "@innoadsstage"
+            // const chat_id = "@innoads";
             const form = req.body;
             const { title, body, price, slug, telegram, categoryId } = form;
             const category = categories.find(
                 (item) => item.value == categoryId
             ).label;
             const images = form.images.split("||");
+            const bodyText = body.length > 200 ? body.substring(0,197) + '...' : body
 
-            //message
-            const text = `<b>${encodeURI(category)}: ${encodeURI(
-                title
-            )}</b> %0A %0A${encodeURI(body)} %0A${encodeURI(
-                "Цена"
-            )}: ${price} %0A${encodeURI(
-                "Подробнее"
-            )}: https://innoads.ru/post/${slug} %0A %0A${encodeURI(
-                "автор"
-            )}: @${telegram}`;
-            const sendMessage = `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage?chat_id=${chat_id}&text=${text}&parse_mode=html`;
-            await axios.get(sendMessage);
+            const text = `Категория: #${category}\nЦена: ${price} \n\n${title} \n\n${bodyText} \n\nПодробнее: https://innoads.ru/post/${slug} \n\nавтор: @${telegram}`;
 
-            //media
             const sendPhoto = `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMediaGroup?chat_id=${chat_id}`;
-            const media = convertLinksToMedia(images);
+
+            const media = convertLinksToMedia(images, text);
             await axios.post(sendPhoto, { media });
 
             return res.json({ status: "success" });
