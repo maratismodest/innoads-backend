135c6194ad749cd20a3cb780ea919f3c8d6b2c3a
Marat Faizerakhmanov <maratismodest@gmail.com>
2022-07-23 22:26:52 +0300

    commit

--- routes/telegramRouter.js
+++ routes/telegramRouter.js
@@ -1,9 +1,10 @@
 const Router = require('express')
 const telegramController = require("../controllers/telegramController");
+const secretMiddleware = require("../middleware/secretMiddleware");
 const router = new Router()
 
 module.exports = router
 
 router.post('/post', telegramController.postTelegram)
-router.post('/site', telegramController.postSite)
+router.post('/site', secretMiddleware, telegramController.postSite)
 router.post('/', telegramController.postUser)
